33fffad962c6248d2c6b10d076dd83ac7e2594d3
cocagolau <cocagolau@gmail.com>
2014-07-10 16:50:47 +0900

    week1

--- week1/FileString/FileString/main.m
+++ week1/FileString/FileString/main.m
@@ -11,10 +11,40 @@
 void NXDisplayAllFilesAtPath(NSString *path) {
     NSFileManager *fm =[NSFileManager defaultManager];
     NSDirectoryEnumerator *dirEnumerator = [fm enumeratorAtPath:path];
+    NSString *itemPath;
+    while ((itemPath = [dirEnumerator nextObject]) != nil) {
+        NSDictionary * dict = [fm attributesOfItemAtPath:itemPath error:nil];
+        [dict ]
+        
+        NSLog(@"%@ -type: %@", itemPath, [dict objectForKey:NSFileType]);
+    }
+}
+
+NSArray * NXAllFilesAtPath(NSString * path) {
+    NSArray * files = [NSArray array];
+    
+    NSFileManager *fm =[NSFileManager defaultManager];
+    NSDirectoryEnumerator *dirEnumerator = [fm enumeratorAtPath:path];
     NSString *item;
     while ((item = [dirEnumerator nextObject]) != nil) {
-        NSLog(@"%@", item);
+        [files arrayByAddingObject:item];
     }
+    
+    return files;
+}
+
+BOOL isExistFilenameAtPath(NSString * filename, NSString * path) {
+    
+    NSFileManager *fm =[NSFileManager defaultManager];
+    NSDirectoryEnumerator *dirEnumerator = [fm enumeratorAtPath:path];
+    NSString *item;
+    while ((item = [dirEnumerator nextObject]) != nil) {
+        if (filename == item) {
+            return true;
+        }
+    }
+    
+    return false;
 }
 
 int main(int argc, const char * argv[])
@@ -23,8 +53,17 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         NSString *path = @"/Users/Dec7/Documents/workspace";
+        NSString *filename = @"HelloWorld.java";
         
         NXDisplayAllFilesAtPath(path);
+        NXAllFilesAtPath(path);
+        
+        if (isExistFilenameAtPath(filename, path)) {
+            NSLog(@"%@ 있음!!", filename);
+        }
+        else {
+            NSLog(@"%@ 없음!!", filename);
+        }
         
     }
     return 0;
