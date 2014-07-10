77d02f8fb2580423692c50c34daf7892d55cb0a3
cocagolau <cocagolau@gmail.com>
2014-07-10 15:08:59 +0900

    Initial Commit

---
+++ week1/FileString/FileString/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FileString
+//
+//  Created by Dec7 on 2014. 7. 10..
+//  Copyright (c) 2014년 NHNNEXT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
